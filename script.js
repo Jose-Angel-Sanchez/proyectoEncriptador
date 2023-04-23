898201c98555782af4a71e555cff4b0ef34a3496
José Angel <Angelsanchezvelez146@gmail.com>
2023-04-23 01:05:57 -0600

    Optimizando código del botón de encriptado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function encriptar() {
 	let texto = document.querySelector('textarea').value;
 	let titulo = document.querySelector("h2");
 	let descripcion = document.querySelector("p");
-	let imagen = document.querySelector("img");
+	let imagen = document.querySelector("div img");
 	let textoCifrado = texto
 	.replace(/e/gi, "enter")
 	.replace(/i/gi, "imes")
@@ -11,11 +11,12 @@ function encriptar() {
 	.replace(/u/gi, "ufat");
 	if (document.querySelector('textarea').value.length > 0) {
 		document.querySelector('textarea').value = textoCifrado;
-		document.querySelector("h2").innerText = "Texto encriptado correctamente.";
-		document.querySelector("p").innerText = textoCifrado;
+		titulo.innerText = "Texto encriptado correctamente:";
+		descripcion.innerText = textoCifrado;
+		imagen.style.display = "none";
 	}
 	else{
-		document.getElementsByClassName("imagen").display = "hide";
-		alert("Debes ingresar al menos un caracter.")
+		document.getElementsByClassName("imagen").src = "/#";
+		alert("Debes ingresar al menos un caracter.");
 	}
 }
